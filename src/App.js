0c1fcec316ad696db8fb8cb02a02335892445d9b
meijiao <meijiao@gmail.com>
2017-02-04 15:33:38 -0800

    Create mock data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import mockData from './data.js';
+
+console.log(mockData);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Team Forecast</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
