1f5e278aa70f41d29be2d3cc0f73c3318d652295
meijiao <meijiao@gmail.com>
2017-02-04 19:26:03 -0800

    repeat row component

--- src/App.js
+++ src/App.js
@@ -2,22 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 import data from './data.js';
 
-function Row (props) {
-  console.log(props);
+function Row({name, closed, commit, forecast, likely}) {
   return (
     <div className="Row">
       <ul>
-        <li className="Row-name">{props.value.name}</li>
-        <li className="Row-closed">{props.value.closed}</li>
-        <li className="Row-commit">{props.value.commit}</li>
-        <li className="Row-forecast">{props.value.forecast}</li>
-        <li className="Row-likely">{props.value.likely}</li>
+        <li className="Row-name">{name}</li>
+        <li className="Row-closed">{closed}</li>
+        <li className="Row-commit">{commit}</li>
+        <li className="Row-forecast">{forecast}</li>
+        <li className="Row-likely">{likely}</li>
       </ul>
     </div>
   )
 }
 
 class App extends Component {
+  
+  createRow(data) {
+    return data.map((person, i) => {
+      return <Row 
+        name={person.name}
+        closed={person.closed} 
+        commit={person.commit}
+        forecast={person.forecast}
+        likely={person.likely}
+        key={i}
+        />
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -25,10 +38,10 @@ class App extends Component {
           <h2>What is Your Team's Sales Forecast?</h2>
         </div>
         <div className="App-summary">
-          <h2> Test</h2>
+          <h2>Test</h2>
         </div>
         <div className="App-table">
-            <Row value={data[0]}/>
+            {this.createRow(data)}
         </div>
       </div>
     );
