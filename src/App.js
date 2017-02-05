0da79c0a2b56fbdeae9072890e3cce8d83a73c35
meijiao <meijiao@gmail.com>
2017-02-05 02:12:44 -0800

    create css for table structure

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import data from './data.js';
 function Row({name, closed, commit, forecast, likely}) {
   return (
     <div className="Row">
-      <ul>
-        <li className="Row-name">{name}</li>
-        <li className="Row-closed">{closed}</li>
-        <li className="Row-commit">{commit}</li>
-        <li className="Row-forecast">{forecast}</li>
-        <li className="Row-likely">{likely}</li>
-      </ul>
+      <div className="Row-name">{name}</div>
+      <div className="Row-dollar">{closed}</div>
+      <div className="Row-dollar">{commit}</div>
+      <div className="Row-dollar">{forecast}</div>
+      <div className="Row-dollar">{likely}</div>
     </div>
   )
 }
@@ -22,13 +20,32 @@ class App extends Component {
     return data.map((person, i) => {
       return <Row 
         name={person.name}
-        closed={person.closed} 
-        commit={person.commit}
-        forecast={person.forecast}
-        likely={person.likely}
+        closed={this.formatCurrency(person.closed)} 
+        commit={this.formatCurrency(person.commit)}
+        forecast={this.formatCurrency(person.forecast)}
+        likely={this.formatCurrency(person.likely)}
         key={i}
         />
     })
+  }
+
+  formatCurrency(amount) {
+    let output = ''
+    let str = String(amount);
+    if(str.length <= 3) {
+      return '$ ' + str;
+    }
+    let counter = 1;
+    for (var i = str.length - 1; i >= 0; i--) {
+      if (counter === 3 && i !==0) {
+        output = ',' + str[i] + output;
+        counter = 0;
+      } else {
+        output = str[i] + output;
+        counter ++;
+      }
+    }
+    return '$ ' + output;
   }
 
   render() {
--- src/data.js
+++ src/data.js
@@ -10,10 +10,10 @@ function create (name, closed, commit, forecast, likely) {
   });
 }
 
-create('John', 100000, 1000000, 547090, 550000);
-create('Alice', 200000, 100000, 570900, 450000);
-create('Peter', 150000, 1600000, 587090, 590000);
-create('Jane', 300000, 1800000, 220090, 600000);
-create('Eve', 400000, 1200000, 123090, 800000);
+create('John', 100000, 100000, 547090, 550000);
+create('Alice', 200000, 10000, 570900, 450000);
+create('Peter', 150000, 160000, 587090, 590000);
+create('Jane', 300000, 180000, 220090, 600000);
+create('Eve', 400000, 120000, 123090, 800000);
 
 export default deals;
